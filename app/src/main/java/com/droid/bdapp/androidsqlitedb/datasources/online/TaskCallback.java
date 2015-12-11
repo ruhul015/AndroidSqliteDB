974ea85a6aadbc7e539ffc0b07015255309a22ae
ruhul015 <email2ruhul@gmail.com>
2015-12-11 15:07:45 +0600

    Added database code

--- app/src/main/java/com/droid/bdapp/androidsqlitedb/datasources/online/TaskCallback.java
+++
@@ -1,8 +0,0 @@
-package com.droid.bdapp.androidsqlitedb.datasources.online;
-
-public interface TaskCallback
-{
-	public void onEndCallback(final TaskType type, final TaskParameter param);
-	public void onCancelCallback(final TaskType type, final TaskParameter param);
-	public void onProgressCallback(final TaskType type, final TaskParameter param);
-}
